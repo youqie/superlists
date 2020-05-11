b975b3b1f6a04f35541e23fd82b50ec274ad8607
youqie <youqie@vip.qq.com>
2020-05-11 23:37:46 +0800

    Page 87 & Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # check homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # notice the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -27,36 +27,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        # self.assertEqual(
-        #     inputbox.get_attribute('placeholder'),
-        #     'Enter a to-do item'
-        # )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page
+        # lists "1:Buy ..." as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
-        # type "Buy peacock feathers" into a text box
-        # inputbox.send_keys('Buy peacock feathers')
 
-        # # hit enter -> page updates -> the page lists this item in a to-do list table
-        # inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
-
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
-        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        # self.assertIn(
-        #     '2: Use peacock feathers to make a fly',
-        #     [row.text for row in rows]
-        # )
-        # finish the test
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
