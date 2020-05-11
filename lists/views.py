884d50fb9362ffaba975295bc5ede4eda09078c5
youqie <youqie@vip.qq.com>
2020-05-11 23:17:43 +0800

    Page 80 finished & Redirect after POST, and show all items in template

--- lists/views.py
+++ lists/views.py
@@ -7,5 +7,5 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/') 
-
-    return render(request,'home.html')
+    items = Item.objects.all()
+    return render(request,'home.html', {'items':items})
