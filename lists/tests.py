f9112fe936ce2177ce512018ddb0c76b1c6e0043
youqie <youqie@vip.qq.com>
2020-05-19 11:02:54 +0800

    Page 146 finished & add a message summarising the above...

--- lists/tests.py
+++ lists/tests.py
@@ -10,19 +10,19 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')
 
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+    #     html = response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html')
+    #     self.assertTemplateUsed(response, 'home.html')
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -38,7 +38,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text':'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -50,8 +50,22 @@ class HomePageTest(TestCase):
 
         response = self.client.get('/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        # self.assertIn('itemey 1', response.content.decode())
+        # self.assertIn('itemey 2', response.content.decode())
+    
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_uses_list_templates(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
 class ItemModelTest(TestCase):
 
