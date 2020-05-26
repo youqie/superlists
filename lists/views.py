f475d38658120a4e7ea4ead7257bc5d443ff2995
youqie <youqie@vip.qq.com>
2020-05-26 10:28:57 +0800

    Page 113

--- lists/views.py
+++ lists/views.py
@@ -4,9 +4,9 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/') 
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/') 
     return render(request,'home.html')
 
 def view_list(request):
